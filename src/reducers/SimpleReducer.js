f1b7b277f4e99346a76a2d53c6642b805e7197c4
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-13 20:31:55 +0100

    WIP: setup simple reducer scaffolding and initialState

---
+++ src/reducers/SimpleReducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+};
+
+export const simpleReducder = (state, action) => {
+  switch (action.type) {
+    case '':
+      return {
+        
+      };
+
+    case '':
+      return {
+       
+      };
+    default:
+      return state;
+  }
+};
